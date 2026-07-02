cb58ec4cf7013e4b7801b4b51732cfbdc980fb54
ele-na2011 <1yangele@hdsb.ca>
2026-07-02 14:31:11 +0000

    added function and figured out why it wasnt working lmao

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkPassword() {
             var password = document.getElementById("passwordInput").value;
             // Setting the password
             if (password === "RebeccaWuIsAnUnc6767") {
-                window.location.href = "secret_page.html"; // The hidden page
+                showMessageScreen(); // The hidden page
             } else {
                 alert("Incorrect password! Try again.");
             }
